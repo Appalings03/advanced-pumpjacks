964cfc9cf9688fee8c9219625b28bc167fc23c38
Martin <97223840+Appalings03@users.noreply.github.com>
2026-03-03 09:24:47 +0100

    Change default value and range for apj-tier-count

--- settings.lua
+++ settings.lua
@@ -3,9 +3,9 @@ data:extend({
     type = "int-setting",
     name = "apj-tier-count",
     setting_type = "startup",
-    default_value = 3,
+    default_value = 2,
     minimum_value = 1,
-    maximum_value = 20,
+    maximum_value = 4,
     order = "a"
   }
 })
