9be273a923e79eb6f8b90a6f172b36b2f16847fb
Martin <97223840+Appalings03@users.noreply.github.com>
2026-03-03 10:17:27 +0100

    Modify settings for tier count and add balance mode
    Updated the apj-tier-count default value and added a new apj-space-age-balance setting.

--- settings.lua
+++ settings.lua
@@ -1,11 +1,27 @@
 data:extend({
+
+  --------------------------------------------------
+  -- BASE TIERS
+  --------------------------------------------------
   {
     type = "int-setting",
     name = "apj-tier-count",
     setting_type = "startup",
-    default_value = 2,
+    default_value = 3,
     minimum_value = 1,
-    maximum_value = 4,
+    maximum_value = 6,
     order = "a"
+  },
+
+  --------------------------------------------------
+  -- SPACE AGE BALANCE MODE
+  --------------------------------------------------
+  {
+    type = "bool-setting",
+    name = "apj-space-age-balance",
+    setting_type = "startup",
+    default_value = true,
+    order = "b",
+    hidden = not mods["space-age"]
   }
 })
