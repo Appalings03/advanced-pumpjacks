ef1c0ee0b56fde1bfa6f6d5c4d1f672c23af4d46
Martin <97223840+Appalings03@users.noreply.github.com>
2026-03-03 16:12:37 +0100

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -5,13 +5,11 @@ if mods["space-age"] then
     for i = max_space_age_tiers + 1, 6 do
         local name = "pumpjack-mk" .. (i + 1)
 
-        -- Supprimer la recette
         if data.raw.recipe[name] then
             data.raw.recipe[name].enabled = false
             data.raw.recipe[name].hidden = true
         end
 
-        -- Supprimer la technologie
         if data.raw.technology[name] then
             data.raw.technology[name].hidden = true
             data.raw.technology[name].enabled = false
